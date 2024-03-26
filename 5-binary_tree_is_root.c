9bbc87fde2e12b4746ba7e5c24a441600b0fafca
Arafa Khalaf <khalafarafa@gmail.com>
2024-03-26 15:00:19 -0700

    Binary Trees | 07

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_leaf - function checks if a given node is a root
+ * binary_tree_is_root - function checks if a given node is a root
  * @node: pointer to the node to check
  * Return: 1 if node is a root, otherwise 0
  *		If node is NULL, return 0
