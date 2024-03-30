cf68093ec111291e1c39ca593537d3614965ed1a
Arafa Khalaf <khalafarafa@gmail.com>
2024-03-30 05:19:19 -0700

    Binary Trees | 29

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -9,18 +9,18 @@
 bst_t *bst_search(const bst_t *tree, int value)
 {
 	if (tree == NULL)
-		return NULL;
+		return (NULL);
 
 	if (value < tree->n)
 	{
-		return bst_search(tree->left, value);
+		return (bst_search(tree->left, value));
 	}
 	else if (value > tree->n)
 	{
-		return bst_search(tree->right, value);
+		return (bst_search(tree->right, value));
 	}
 	else /* value == tree->n */
 	{
-		return (bst_t *)tree;
+		return ((bst_t *)tree);
 	}
 }
